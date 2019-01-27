77598272520597b7c63c293f56d0952051a2d848
Marcel Steffen <marcel@talentsconnect.com>
2019-01-27 22:03:35 +0100

    use path param in component

--- src/components/views/Day.jsx
+++ src/components/views/Day.jsx
@@ -5,10 +5,11 @@ import {Link} from 'react-router-dom'
 export default class Day extends Component {
 
     render() {
+        const dayNumber = parseInt(this.props.match.params.number);
         return (
             <div>
                 <Link to='/'>back</Link>
-                <h1>Day</h1>
+                <h1>Day {dayNumber}</h1>
             </div>
         );
     }
